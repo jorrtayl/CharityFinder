9c19a98dbc5474f2edbae2132abd9778e84afa65
Zach Coomer <zaccoom@ut.utm.edu>
2024-10-26 17:01:57 -0500

    updated dependencies to use modules esm vs commonjs, added database.js to house database queries

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+import express from'express'
+import bodyParser from 'body-parser'
+import cors from 'cors';
+import axios from 'axios'
 
 const ProPublicaURL = "https://projects.propublica.org/nonprofits/api/v2/";
 
