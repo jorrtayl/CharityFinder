109da98093c07d0322a9d5f943cb2ff0102b0ebe
Zach Coomer <zaccoom@ut.utm.edu>
2024-11-02 19:11:20 -0500

    added nodemon script to server.js and some generic queries in database.js

--- backend/database.js
+++ backend/database.js
@@ -15,5 +15,29 @@ async function getCharities() {
     return rows
 }
 
-const charities = await getCharities()
-console.log(charities)
+// const charities = await getCharities()
+// console.log(charities)
+
+async function getCharity(charity_id){
+    const [rows] = await pool.query(`
+        SELECT *
+        FROM charities
+        WHERE charity_id =?
+        `, [charity_id])
+        return rows
+}
+
+// const charity = await getCharity(1)
+// console.log(charity)
+
+async function getCharityByEin(ein){
+    const [rows] = await pool.query(`
+        SELECT *
+        FROM charities
+        WHERE ein =?
+        `, [ein])
+        return rows
+}
+
+const charity = await getCharityByEin("131788491")
+console.log(charity)
